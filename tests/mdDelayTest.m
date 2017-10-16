b482c2c122d0c35ff273835a922af9071f9ff524
Dan Mønster <dan@m0nster.dk>
2017-10-16 10:08:26 +0200

    Initial test of mdDelay()

---
+++ tests/mdDelayTest.m
@@ -0,0 +1,44 @@
+% Run these tests with: runtests('mdDelayTest.m')
+
+function tests = mdDelayTest
+tests = functiontests(localfunctions);
+end
+
+function setupOnce(testCase)
+% Add the directory above the test directory to search path
+pathToAdd = strcat(pwd(), '/..');
+addpath(pathToAdd, '-end');
+% path
+end
+
+function teardownOnce(testCase)
+disp("Teardown called. No action taken here.")
+end
+
+function testUniVariate(testCase)
+x = [1, 2, 1, 2, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]';
+actTau = mdDelay(x, 'plottype', 'all');
+expTau = 6;
+verifyEqual(testCase, actTau, expTau);
+end
+
+function testBiVariate(testCase)
+x = [1, 2, 1, 2, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]';
+y = [7, 6, 5, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6]';
+actTau = mdDelay([x,y]);
+expTau = 6;
+verifyEqual(testCase, actTau, expTau);
+end
+
+function testMultivariateSine(testCase)
+nDataPoints = 100;
+intervalLength = 2*pi;
+nVariables = 5;
+x = zeros(nDataPoints, nVariables);
+for var=1:nVariables
+    x(:,var) = sin((1:nDataPoints)/nDataPoints*intervalLength + var);
+end
+acttau = mdDelay(x);
+exptau = 3;
+verifyEqual(testCase, acttau, exptau);
+end
